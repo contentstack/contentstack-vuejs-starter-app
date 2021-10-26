5b497a3ef881e57cee5b67e07696611ccb677c6b
Amitkanswal <amit.kanswal@contentstack.com>
2021-10-26 21:01:48 +0530

    fix:removed unwanted property from response and add css fix [ECO-188]

--- src/components/Devtools.vue
+++ src/components/Devtools.vue
@@ -20,15 +20,17 @@
           />
         </div>
         <div class="modal-body">
-          <pre id="jsonViewer"></pre>
+          <pre id="jsonViewer">
+            <json-viewer id="jsonViewer">{{response}}</json-viewer>
+          </pre>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import JsonViewer from 'json-viewer-js';
-
+// import JsonViewer from 'json-viewer-js';
+import '@alenaksu/json-viewer';
 export default {
   computed: {
     response() {
@@ -39,21 +41,47 @@ export default {
       };
       page && (response.page = page);
       blogPost && (response.blog_post = blogPost);
-      return JSON.stringify(response);
+      const jsonData = this.filterObject(response);
+      return jsonData;
     }
   },
-  mounted() {
-    const jsonViewer = document.getElementById('jsonViewer');
-    try {
-      new JsonViewer({
-        container: jsonViewer,
-        data: this.response,
-        theme: 'light',
-        expand: false
-      });
-    } catch (error) {
-      console.error('error', error);
+  methods: {
+    filterObject: function(inputObject) {
+      const unWantedProps = [
+        'uid',
+        '_version',
+        'ACL',
+        '_in_progress',
+        'created_at',
+        'created_by',
+        'updated_at',
+        'updated_by',
+        'publish_details'
+      ];
+      for (const key in inputObject) {
+        unWantedProps.includes(key) && delete inputObject[key];
+        if (typeof inputObject[key] !== 'object') {
+          continue;
+        }
+        inputObject[key] = this.filterObject(inputObject[key]);
+      }
+      return inputObject;
     }
+  },
+  mounted: function mounted() {
+    // const jsonViewer = document.getElementById('jsonViewer');
+    let jsonData = this.filterObject(this.response);
+    this.response = JSON.stringify(jsonData);
+    // try {
+    //   new JsonViewer({
+    //     container: jsonViewer,
+    //     data: jsonData,
+    //     theme: 'light',
+    //     expand: false
+    //   });
+    // } catch (error) {
+    //   console.error('error', error);
+    // }
   }
 };
 </script>
