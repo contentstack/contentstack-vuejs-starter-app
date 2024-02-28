9aca4bad6c7ae46dc663e7967f2e6577e52b8b0b
Ankita Dodamani <ankita.dodamani@contentstack.com>
2024-02-28 18:32:46 +0530

    MKT-4822 Config updated to overwrite the variables if necessary

--- vue.config.ts
+++ vue.config.ts
@@ -1,5 +1,24 @@
 import { defineConfig } from '@vue/cli-service'
 
+// Map envs with VUE_APP_ prefix only if CONTENTSTACK_ variable is undefined for launch integration
+(function () {
+  if (typeof process.env.CONTENTSTACK_API_KEY === 'undefined') {
+    process.env.VUE_APP_CONTENTSTACK_API_KEY = process.env.CONTENTSTACK_API_KEY;
+  }
+  if (typeof process.env.CONTENTSTACK_API_HOST === 'undefined') {
+    process.env.VUE_APP_CONTENTSTACK_API_HOST = process.env.CONTENTSTACK_API_HOST;
+  }
+  if (typeof process.env.CONTENTSTACK_DELIVERY_TOKEN === 'undefined') {
+    process.env.VUE_APP_CONTENTSTACK_DELIVERY_TOKEN = process.env.CONTENTSTACK_DELIVERY_TOKEN;
+  }
+  if (typeof process.env.CONTENTSTACK_ENVIRONMENT === 'undefined') {
+    process.env.VUE_APP_CONTENTSTACK_ENVIRONMENT = process.env.CONTENTSTACK_ENVIRONMENT;
+  }
+  if (typeof process.env.CONTENTSTACK_CDN === 'undefined') {
+    process.env.VUE_APP_CONTENTSTACK_CDN = process.env.CONTENTSTACK_CDN;
+  }
+})();
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
